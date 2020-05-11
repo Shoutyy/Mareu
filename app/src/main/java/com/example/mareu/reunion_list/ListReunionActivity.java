e617443e4c050a5b97647407c8382b7f6d7bff09
Benjamin Debray <shouty91600@hotmail.fr>
2020-05-11 11:13:16 +0200

    update

--- app/src/main/java/com/example/mareu/reunion_list/ListReunionActivity.java
+++ app/src/main/java/com/example/mareu/reunion_list/ListReunionActivity.java
@@ -1,20 +1,28 @@
 package com.example.mareu.reunion_list;
 
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import com.example.mareu.R;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ListReunionActivity extends AppCompatActivity {
+public class ListReunionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -25,8 +33,10 @@ public class ListReunionActivity extends AppCompatActivity {
     @BindView(R.id.item_filter_reunion)
     public ImageButton mFiltreReunion;
 
+    Boolean visibility;
     ListReunionPagerAdapter mPagerAdapter;
-
+    EditText date;
+    DatePickerDialog datePickerDialog;
 
 
 
@@ -47,12 +57,70 @@ public class ListReunionActivity extends AppCompatActivity {
                 startActivity(ajoutReunionActivityIntent);
             }
         });
+
+        visibility = false;
+        // initiate the date picker and a button
+        date = findViewById(R.id.date);
+        // perform click event on edit text
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR); // current year
+                int mMonth = calendar.get(Calendar.MONTH); // current month
+                int mDay = calendar.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(ListReunionActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText(dayOfMonth + "/"
+                                        + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+
+        Spinner spinner = findViewById(R.id.salleSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.arraySalle,R.layout.spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(R.layout.spinner_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         mFiltreReunion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
-                Intent ajoutReunionActivityIntent = new Intent(ListReunionActivity.this, AjoutReunionActivity.class);
-                startActivity(ajoutReunionActivityIntent);
+                if(visibility == false )
+                {
+                    date.setVisibility(view.VISIBLE);
+                    spinner.setVisibility(view.VISIBLE);
+                    visibility = true;
+                }
+                else
+                {
+                    visibility = false;
+                    date.setVisibility(view.GONE);
+                    spinner.setVisibility(view.GONE);
+                }
             }
         });
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        CharSequence charSequence = (CharSequence) parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
